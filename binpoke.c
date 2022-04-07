72fc76506996218360f0a8839517f745bef2dcfb
Noah Johnson <noah.johnson@loupmail.com>
2022-04-07 14:45:49 -0500

    Implement list verb

--- binpoke.c
+++ binpoke.c
@@ -26,6 +26,51 @@
 #endif
 
 /*
+ * Constants
+ * =========
+ */
+
+/*
+ * The maximum number of bytes that may be listed.
+ * 
+ * This is just to prevent a gigantic listing.  It is set here to 64K.
+ */
+#define LIST_MAXBYTES (INT64_C(65536))
+
+/*
+ * Type declarations
+ * =================
+ */
+
+/*
+ * Structure that stores a single line of data for a listing.
+ */
+typedef struct {
+  
+  /*
+   * The paragraph number to display.
+   * 
+   * This is the 32 least significant bytes of the address, divided by
+   * 16.
+   */
+  int32_t para;
+  
+  /*
+   * The unsigned byte values to display.
+   * 
+   * This array has one element for each byte of the paragraph.
+   * 
+   * Elements that are present must be in range [0, 255].
+   * 
+   * You can also use negative values for elements that are not present,
+   * such as occurs if you have a partial line that goes outside the
+   * boundaries of the range being listed.
+   */
+  int bv[16];
+  
+} LIST_LINE;
+
+/*
  * Local data
  * ==========
  */
@@ -45,8 +90,11 @@ const char *pModule = NULL;
 /* Prototypes */
 static void fault(int line);
 static void printInt64(int64_t v);
+static void printListLine(const LIST_LINE *pl);
 
 static int64_t parseCount(const char *pstr);
+static int parseHex(const char *pstr, uint64_t *pv);
+static int64_t parseAddress(const char *pstr);
 
 static int verb_list(
     const char *pPath,
@@ -130,6 +178,77 @@ static void printInt64(int64_t v) {
 }
 
 /*
+ * Print the contents of a listing line structure to standard output.
+ * 
+ * Parameters:
+ * 
+ *   pl - the structure to print
+ */
+static void printListLine(const LIST_LINE *pl) {
+  
+  int i = 0;
+  
+  /* Check parameter */
+  if (pl == NULL) {
+    fault(__LINE__);
+  }
+  
+  /* Check paragraph number and print address field */
+  if ((pl->para < 0) || (pl->para > INT32_C(0xfffffff))) {
+    fault(__LINE__);
+  }
+  printf("%07lx0:", (long) pl->para);
+  
+  /* Print each byte value */
+  for(i = 0; i < 16; i++) {
+    /* Space is separator, except before element 8 there are three
+     * spaces */
+    if (i == 8) {
+      printf("   ");
+    } else {
+      putchar(' ');
+    }
+    
+    /* Range check byte value and print */
+    if (((pl->bv)[i] >= 0) && ((pl->bv)[i] <= 255)) {
+      /* Unsigned byte value */
+      printf("%02x", (pl->bv)[i]);
+      
+    } else if ((pl->bv)[i] < 0) {
+      /* Missing byte value */
+      printf("  ");
+      
+    } else {
+      /* Invalid byte value */
+      fault(__LINE__);
+    }
+  }
+  
+  /* Print separator between bytes and characters */
+  printf(" | ");
+  
+  /* Print each character value */
+  for(i = 0; i < 16; i++) {
+    /* Range check byte value and print */
+    if (((pl->bv)[i] >= 0x20) && ((pl->bv)[i] <= 0x7e)) {
+      /* US-ASCII printing character */
+      putchar((pl->bv)[i]);
+      
+    } else if ((pl->bv)[i] < 0) {
+      /* Missing byte value */
+      putchar(' ');
+      
+    } else {
+      /* Neither missing nor US-ASCII */
+      putchar('.');
+    }
+  }
+  
+  /* Finally, print the line break */
+  printf("\n");
+}
+
+/*
  * Parse a 64-bit count parameter from a given string.
  * 
  * These strings are unsigned decimal strings that must be in range of
@@ -200,14 +319,291 @@ static int64_t parseCount(const char *pstr) {
 }
 
 /*
- * @@TODO:
+ * Parse a sequence of base-16 digits as an unsigned value.
+ * 
+ * pstr must point to a sequence of at least one base-16 character.
+ * 
+ * The result will be stored in *pv.  If parsing fails, an undefined
+ * value will be in *pv.  Check the return to determine whether parsing
+ * succeeded or not.
+ * 
+ * Parameters:
+ * 
+ *   pstr - the string to parse
+ * 
+ *   pv - pointer to variable to receive parsed result
+ * 
+ * Return:
+ * 
+ *   non-zero if successful, zero if error
+ */
+static int parseHex(const char *pstr, uint64_t *pv) {
+  
+  int status = 1;
+  int d = 0;
+  
+  /* Check parameters */
+  if ((pstr == NULL) || (pv == NULL)) {
+    fault(__LINE__);
+  }
+  
+  /* Reset result to zero */
+  *pv = 0;
+  
+  /* Fail if empty string */
+  if (*pstr == 0) {
+    status = 0;
+  }
+  
+  /* Parse all digits */
+  if (status) {
+    for( ; *pstr != 0; pstr++) {
+      /* Get current digit value */
+      if ((*pstr >= '0') && (*pstr <= '9')) {
+        d = *pstr - '0';
+      } else if ((*pstr >= 'a') && (*pstr <= 'f')) {
+        d = *pstr - 'a' + 10;
+      } else if ((*pstr >= 'A') && (*pstr <= 'F')) {
+        d = *pstr - 'A' + 10;
+      } else {
+        status = 0;
+      }
+      
+      /* Multiply result by 16, watching for overflow */
+      if (status) {
+        if (*pv <= UINT64_MAX / 16) {
+          *pv *= 16;
+        } else {
+          status = 0;
+        }
+      }
+      
+      /* Add digit into result, watching for overflow */
+      if (status) {
+        if (*pv <= UINT64_MAX - d) {
+          *pv += d;
+        } else {
+          status = 0;
+        }
+      }
+      
+      /* Leave loop if error */
+      if (!status) {
+        break;
+      }
+    }
+  }
+  
+  /* Return status */
+  return status;
+}
+
+/*
+ * Parse an address encoded within a given string.
+ * 
+ * If the given string does not begin with "0x" or "0X" then this call
+ * is equivalent to parseCount().
+ * 
+ * If the given string begins with "0x" or "0X" then parseHex() is
+ * called on the rest of the string after the prefix.  If the unsigned
+ * value returned from that is within 64-bit signed range, then that is
+ * the result; otherwise, the function returns a failure code of -1.
+ * 
+ * Parameters:
+ * 
+ *   pstr - the string to parse
+ * 
+ * Return:
+ * 
+ *   the parsed address, or -1 if parsing error
+ */
+static int64_t parseAddress(const char *pstr) {
+  
+  int64_t result = 0;
+  uint64_t uv = 0;
+  
+  /* Check parameter */
+  if (pstr == NULL) {
+    fault(__LINE__);
+  }
+  
+  /* Handle cases */
+  if (strlen(pstr) < 2) {
+    /* Less than two characters in string, so no possible way there
+     * could be a prefix; call through to parseCount() */
+    result = parseCount(pstr);
+    
+  } else if ((pstr[0] != '0') ||
+                ((pstr[1] != 'x') && (pstr[1] != 'X'))) {
+    /* Does not begin with a prefix, so call through to parseCount() */
+    result = parseCount(pstr);
+    
+  } else {
+    /* If we got here, we have a prefix, so parse everything after the
+     * prefix as a hex string */
+    if (parseHex(pstr + 2, &uv)) {
+      /* We now have the unsigned 64-bit value; check whether in
+       * range */
+      if (uv <= INT64_MAX) {
+        /* In range, so cast to signed to get result */
+        result = (int64_t) uv;
+        
+      } else {
+        /* Not in range, so parsing failed */
+        result = -1;
+      }
+      
+    } else {
+      /* Parsing failed */
+      result = -1;
+    }
+  }
+  
+  /* Return result or -1 */
+  return result;
+}
+
+/*
+ * Verb to generate a hex dump listing.
+ * 
+ * Parameters:
+ * 
+ *   pPath - path to the file
+ * 
+ *   pFrom - string parameter with starting address
+ * 
+ *   pFor - string parameter with byte count
+ * 
+ * Return:
+ * 
+ *   non-zero if successful, zero if error
  */
 static int verb_list(
     const char *pPath,
     const char *pFrom,
     const char *pFor) {
-  fprintf(stderr, "verb_list %s from %s for %s\n", pPath, pFrom, pFor);
-  return 1;
+  
+  int status = 1;
+  int errcode = 0;
+  AKSVIEW *pv = NULL;
+  
+  int64_t addr = 0;
+  int64_t count = 0;
+  
+  int i = 0;
+  int64_t p = 0;
+  int64_t p_first = 0;
+  int64_t p_last = 0;
+  
+  LIST_LINE ls;
+  
+  /* Initialize structures */
+  memset(&ls, 0, sizeof(LIST_LINE));
+  
+  /* Check parameters */
+  if ((pPath == NULL) || (pFrom == NULL) || (pFor == NULL)) {
+    fault(__LINE__);
+  }
+  
+  /* Get the address */
+  addr = parseAddress(pFrom);
+  if (addr < 0) {
+    status = 0;
+    fprintf(stderr, "%s: Failed to parse address: %s\n",
+              pModule, pFrom);
+  }
+  
+  /* Get the count */
+  if (status) {
+    count = parseCount(pFor);
+    if (count < 0) {
+      status = 0;
+      fprintf(stderr, "%s: Failed to parse count: %s\n",
+                pModule, pFor);
+    }
+  }
+  
+  /* Check that count is at least one and does not exceed limit */
+  if (status && (count < 1)) {
+    status = 0;
+    fprintf(stderr, "%s: Count may not be less than one!\n", pModule);
+  }
+  
+  if (status && (count > LIST_MAXBYTES)) {
+    status = 0;
+    fprintf(stderr, "%s: Count exceeds LIST_MAXBYTES limit!\n",
+              pModule);
+  }
+  
+  /* Open a read-only view */
+  if (status) {
+    pv = aksview_create(pPath, AKSVIEW_READONLY, &errcode);
+    if (pv == NULL) {
+      status = 0;
+      fprintf(stderr, "%s: Failed to open file: %s\n",
+                pModule, aksview_errstr(errcode));
+    }
+  }
+  
+  /* Check that address is within file limits */
+  if (status) {
+    if (addr >= aksview_getlen(pv)) {
+      status = 0;
+      fprintf(stderr, "%s: Given address is outside file limits!\n",
+                pModule);
+    }
+  }
+  
+  /* Check that address added to count does not exceed file length */
+  if (status) {
+    if (addr + count > aksview_getlen(pv)) {
+      status = 0;
+      fprintf(stderr, "%s: Given byte range goes beyond end of file!\n",
+                pModule);
+    }
+  }
+  
+  /* Compute the first and last paragraph addresses that will be
+   * displayed in the listing */
+  if (status) {
+    p_first = addr / 16;
+    p_last  = (addr + count - 1) / 16;
+    
+    p_first *= 16;
+    p_last  *= 16;
+  }
+  
+  /* Print a listing of each paragraph */
+  if (status) {
+    for(p = p_first; p <= p_last; p += 16) {
+    
+      /* Write the paragraph number into the structure */
+      ls.para = (int32_t) ((p & INT64_C(0xffffffff)) / 16);
+      
+      /* Read each relevant byte, filling -1 for bytes outside the
+       * requested range */
+      for(i = 0; i < 16; i++) {
+        if (((p + ((int64_t) i)) >= addr) &&
+              ((p + ((int64_t) i)) < addr + count)) {
+          /* Byte is in range */
+          (ls.bv)[i] = (int) aksview_read8u(pv, p + ((int64_t) i));
+          
+        } else {
+          /* Byte is not in range */
+          (ls.bv)[i] = -1;
+        }
+      }
+    
+      /* Print the listing line */
+      printListLine(&ls);
+    }
+  }
+  
+  /* Close viewer if open */
+  aksview_close(pv);
+  
+  /* Return status */
+  return status;
 }
 
 /*
